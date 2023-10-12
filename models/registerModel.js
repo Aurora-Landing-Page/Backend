79e2e3d567e8b9c7e79103a4bae62b6425aa66d5
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2023-10-12 23:04:56 +0530

    new validators

--- models/registerModel.js
+++ models/registerModel.js
@@ -1,28 +1,36 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = mongoose.Schema(
   {
     first_name: {
       type: String,
-      require: [true, "please enter your first name"],
+      required: [true, "please enter your first name"],
+      maxLength: [20, "First Name cannot exceed 30 characters"],
+      minLength: [3, "First Name should have more than 2 characters"],
+
     },
 
     last_name: {
-        type: String,
-        require: [true, "please enter your last name"],
-      },
+      type: String,
+      required: [true, "please enter your last name"],
+      maxLength: [20, "Last Name cannot exceed 20 characters"],
+
+    },
 
     email: {
       type: String,
-      require: [true, "please enter you email"],
+      required: [true, "please enter you email"],
       unique: [true, "email address already taken"],
+      validate: [validator.isEmail, "Please Enter a valid Email"],
+
     },
 
     phone: {
-        type: Number,
-        require: [true, "please enter you phone number"],
-        unique: [true, "phone number already taken"],
-      },
+      type: Number,
+      required: [true, "please enter you phone number"],
+      unique: [true, "phone number already taken"],
+    },
 
   },
   { timestamps: true }
