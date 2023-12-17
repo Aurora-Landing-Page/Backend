1cf25c11a4ccb16caada1b1cd3dbde4403ab1b2b
Nilanjan Mitra <58515762+DarkPhoenix645@users.noreply.github.com>
2023-12-17 13:20:56 +0530

    Added the responsive mail

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -340,7 +340,7 @@ const sendSignupMail = async (req, res) => {
         console.log(error);
         reject(error);
       } else {
-        console.log("email sent: " + info.response);
+        console.log("Email sent: " + info.response);
         resolve(info);
       }
     });
@@ -348,7 +348,202 @@ const sendSignupMail = async (req, res) => {
   res.status(200).send({ message: "Mail sent!" });
 };
 
-// To be implemented
-const sendForgotPassMail = async (email, code) => { console.log(`Email request recieved \n${ email } \n${ code }`); return; }
+// Actually sending out the email has not been tested as I do not have the creds
+// But console.log() on line 354 signifies that this function is called correctly and forgotPass logic in userController.js works
+const sendForgotPassMail = async (email, name, code) => {
+    console.log(email, name, code);
+    var mailOptions = {
+      from: process.env.EMAIL,
+      to: email,
+      subject: "Aurora 2024: Password Reset",
+      html: `<!doctype html>
+      <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+      
+      <head>
+        <title>
+        </title>
+        <!--[if !mso]><!-->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!--<![endif]-->
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style type="text/css">
+          #outlook a {
+            padding: 0;
+          }
+      
+          body {
+            margin: 0;
+            padding: 0;
+            -webkit-text-size-adjust: 100%;
+            -ms-text-size-adjust: 100%;
+          }
+      
+          table,
+          td {
+            border-collapse: collapse;
+            mso-table-lspace: 0pt;
+            mso-table-rspace: 0pt;
+          }
+      
+          img {
+            border: 0;
+            height: auto;
+            line-height: 100%;
+            outline: none;
+            text-decoration: none;
+            -ms-interpolation-mode: bicubic;
+          }
+      
+          p {
+            display: block;
+            margin: 13px 0;
+          }
+        </style>
+        <!--[if mso]>
+              <noscript>
+              <xml>
+              <o:OfficeDocumentSettings>
+                <o:AllowPNG/>
+                <o:PixelsPerInch>96</o:PixelsPerInch>
+              </o:OfficeDocumentSettings>
+              </xml>
+              </noscript>
+              <![endif]-->
+        <!--[if lte mso 11]>
+              <style type="text/css">
+                .mj-outlook-group-fix { width:100% !important; }
+              </style>
+              <![endif]-->
+        <!--[if !mso]><!-->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,700" rel="stylesheet" type="text/css">
+        <style type="text/css">
+          @import url(https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,700);
+        </style>
+        <!--<![endif]-->
+        <style type="text/css">
+          @media only screen and (min-width:480px) {
+            .mj-column-per-100 {
+              width: 100% !important;
+              max-width: 100%;
+            }
+          }
+        </style>
+        <style media="screen and (min-width:480px)">
+          .moz-text-html .mj-column-per-100 {
+            width: 100% !important;
+            max-width: 100%;
+          }
+        </style>
+        <style type="text/css">
+          @media only screen and (max-width:480px) {
+            table.mj-full-width-mobile {
+              width: 100% !important;
+            }
+      
+            td.mj-full-width-mobile {
+              width: auto !important;
+            }
+          }
+        </style>
+      </head>
+      
+      <body style="word-spacing:normal;background-color:#121212;">
+        <div style="background-color:#121212;">
+          <!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+          <div style="margin:0px auto;max-width:600px;">
+            <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;">
+              <tbody>
+                <tr>
+                  <td style="direction:ltr;font-size:0px;padding:20px 0;padding-bottom:20px;padding-top:20px;text-align:center;">
+                    <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:middle;width:600px;" ><![endif]-->
+                    <div class="mj-column-per-100 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:middle;width:100%;">
+                      <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:middle;" width="100%">
+                        <tbody>
+                          <tr>
+                            <td align="center" style="font-size:0px;padding:25px;word-break:break-word;">
+                              <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:collapse;border-spacing:0px;">
+                                <tbody>
+                                  <tr>
+                                    <td style="width:550px;">
+                                      <img height="auto" src="https://i.ibb.co/NTf2s96/Aurora-Email-Logo.png" style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:13px;" width="550" />
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                    <!--[if mso | IE]></td></tr></table><![endif]-->
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <!--[if mso | IE]></td></tr></table><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" bgcolor="#121212" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+          <div style="background:#121212;background-color:#121212;margin:0px auto;max-width:600px;">
+            <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:#121212;background-color:#121212;width:100%;">
+              <tbody>
+                <tr>
+                  <td style="direction:ltr;font-size:0px;padding:20px 0;padding-bottom:20px;padding-top:20px;text-align:center;">
+                    <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:middle;width:600px;" ><![endif]-->
+                    <div class="mj-column-per-100 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:middle;width:100%;">
+                      <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:middle;" width="100%">
+                        <tbody>
+                          <tr>
+                            <td align="center" style="font-size:0px;padding:10px 25px;padding-right:25px;padding-left:25px;word-break:break-word;">
+                              <div style="font-family:open Sans Helvetica, Arial, sans-serif;font-size:16px;line-height:1;text-align:center;color:white;"><span>Hello, ${ name }</span></div>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td align="center" style="font-size:0px;padding:10px 25px;padding-right:25px;padding-left:25px;word-break:break-word;">
+                              <div style="font-family:open Sans Helvetica, Arial, sans-serif;font-size:16px;line-height:1;text-align:center;color:white;">Your password has been reset, please use the new password to login:</div>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td align="center" style="font-size:0px;padding:10px 25px;word-break:break-word;">
+                              <div style="font-family:open Sans Helvetica, Arial, sans-serif;font-size:24px;font-weight:bold;letter-spacing:7px;line-height:1;text-align:center;color:#864ecf;">${ code }</div>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td align="center" style="font-size:0px;padding:10px 25px;padding-right:16px;padding-left:25px;word-break:break-word;">
+                              <div style="font-family:open Sans Helvetica, Arial, sans-serif;font-size:16px;line-height:1;text-align:center;color:white;">If you didn't request this, you can ignore this email or let us know.</div>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td align="center" style="font-size:0px;padding:10px 25px;padding-right:25px;padding-left:25px;word-break:break-word;">
+                              <div style="font-family:open Sans Helvetica, Arial, sans-serif;font-size:16px;line-height:1;text-align:center;color:white;">Aurora Team</div>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                    <!--[if mso | IE]></td></tr></table><![endif]-->
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <!--[if mso | IE]></td></tr></table><![endif]-->
+        </div>
+      </body>
+      
+      </html>`,
+    };
+    await new Promise((resolve, reject) => {
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+          reject(error);
+        } else {
+          console.log("Email sent: " + info.response);
+          resolve(info);
+        }
+      });
+    });
+    res.status(200).send({ message: "Mail sent!" });
+}
 
 module.exports = { sendSignupMail, sendForgotPassMail };
