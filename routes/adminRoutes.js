b484d385fba4173090707a778dab63be9efed0fc
Nilanjan Mitra <58515762+DarkPhoenix645@users.noreply.github.com>
2024-01-10 19:58:14 +0530

    Comment out unfinished endpoints

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -12,7 +12,7 @@ const { requireAdmin } = require("../middlewares/requireAuth")
 router.post("/addIndividual", requireAdmin, eventController.addIndividual);
 router.post("/addGroup", requireAdmin, eventController.addGroup);
 router.get("/getParticipants", requireAdmin, eventController.getParticipants);
-router.get("/generateCaCode", requireAdmin, userController.generateCaCode);
+// router.get("/generateCaCode", requireAdmin, userController.generateCaCode);
 router.get("/verify", requireAdmin, eventController.verify);
 router.post("/attended", requireAdmin, eventController.hasAttended);
 
